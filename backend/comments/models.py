99ba2e47343bc041503449c05a64898d05729488
Bryan Hite <bhite2@mail.usf.edu>
2023-03-17 09:52:07 -0400

    forgot to save model.

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -3,3 +3,9 @@ from authentication.models import User
 
 # Create your models here.
 
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=30)
+    text = models.CharField(max_length=100)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
