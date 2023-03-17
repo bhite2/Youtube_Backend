ac253ef47b01a51934854bfd95d06f712730da02
Bryan Hite <bhite2@mail.usf.edu>
2023-03-17 12:18:16 -0400

    Made migrations + wrote GET and POST API calls

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -3,8 +3,14 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Comment
-# from .serializers import CommentSerializer
+from .serializers import CommentSerializer
 
 
 # Create your views here.
 
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments(request):
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
--- backend/replies/models.py
+++ backend/replies/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from authentication.models import User
-
+from comments.models import Comment
 # Create your models here.
 
-class Comment(models.Model):
+class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(User, on_delete=models.CASCADE)
-    video_id = models.CharField(max_length=30)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
