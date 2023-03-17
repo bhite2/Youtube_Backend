9a0115d1110f59f1e82f7752090ac26d3c0bcb8e
Bryan Hite <bhite2@mail.usf.edu>
2023-03-17 10:04:17 -0400

    Created replies model

--- backend/replies/models.py
+++ backend/replies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=30)
