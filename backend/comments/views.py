3e00e7cf5a253486fc49da419a682cc6fa775665
Bryan Hite <bhite2@mail.usf.edu>
2023-03-17 10:37:16 -0400

    Created and updated urls

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -2,8 +2,9 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
-from .models import Car
-from .serializers import CarSerializer
+from .models import Comment
+# from .serializers import CommentSerializer
 
 
 # Create your views here.
+
